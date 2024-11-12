8b9a0aabf4235391d889caebfd95f4032505010c
John Jang <jscolbe9675@gmail.com>
2024-11-12 16:42:16 -0500

    adding the backend address

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy: {
-    //   "/api": {
-    //     target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
-    //     changeOrigin: true,
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
 });
