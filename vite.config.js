89ac32bb3f01c10451cfdf0c516fc52c613ccf0b
John Jang <jscolbe9675@gmail.com>
2024-11-12 17:14:57 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   server: {
+    port: process.env.PORT || 3000,
     proxy: {
       "/api": {
         target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
