77f961463c97d64bc9e99ee3c73514d9b6bfa7dd
John Jang <jscolbe9675@gmail.com>
2024-11-13 16:49:53 -0500

    forgot to save the changes

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,13 +12,16 @@ export default function Contact() {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("http://localhost:3001/api/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}}/api/contact`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
       if (response.ok) {
         navigate("/received");
