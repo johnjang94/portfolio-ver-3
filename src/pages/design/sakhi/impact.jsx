d1874a1c25116f280f9e29437fa62ab863998a5a
John Jang <jscolbe9675@gmail.com>
2024-12-19 17:09:20 -0500

    adding impact on the sakhi

--- src/pages/design/sakhi/impact.jsx
+++ src/pages/design/sakhi/impact.jsx
@@ -1,13 +1,15 @@
 export default function Impact() {
   return (
     <div>
-      <h3 className="text-xl uppercase">Usability Testing</h3>
+      <h3 className="text-xl uppercase">Impact</h3>
       <div className="md:mx-20 my-3">
         <p className="px-5">
-          We had two different types of usability testing for version 1 and
-          version 3. In general, Version 3 had longer engagement time of 3
-          minutes compared to Version 1 as well as better task completion rate
-          (which is 30%) in comparison to Version 1.
+          Based on records from Maze and additional interviews conducted after
+          usability testing on the new design — which focused on how users
+          interact with buttons to control product quantities, add items to the
+          shopping cart, and place orders — we received{" "}
+          <span className="text-blue-500">30% more positive feedback</span> on
+          the platform redesign.
         </p>
       </div>
     </div>
