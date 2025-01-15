969aafb31cabc7816f16aa6d77624ff0224a0b56
John Jang <jscolbe9675@gmail.com>
2025-01-15 16:54:29 -0500

    modified retrospective part of operate

--- src/pages/design/operate/retrospective.jsx
+++ src/pages/design/operate/retrospective.jsx
@@ -8,16 +8,21 @@ export default function Retrospective() {
             Best Lesson 1 | The Subscription Model
           </li>
           <p className="px-5">
-            In this project, all the services were offered as a package. As I am
-            not a business-person yet, it was a good opportunity to find out
-            what a business model looks like and how it works in reality.
+            In this project, all the services were offered as a subscription
+            basis. As I am not a business-person yet, it was a good opportunity
+            to find out what a business model looks like and how it works in
+            reality.
           </p>
           <li className="text-blue-500">Best Lesson 2 | The Pivot</li>
           <p className="px-5">
-            The pivot was a good choice because we were able to keeping our own
-            statement that we will keep the product easy-to-use. If we decided
-            to combine everything in all-in-one solution, I think that we might
-            have ended up complicating activities.
+            Earlier, I mentioned that I made some changes in my design during
+            mid-fidelity. The reason for the change was that we came to realize
+            how complex the service becomes when we combine accommodation of the
+            rooms and the catering services. The pivot was a good choice because
+            we were able to keeping our own statement that we will keep the
+            product easy-to-use. If we decided to combine everything in
+            all-in-one solution, I think that we might not have been able to
+            have the users clearly understand how things work.
           </p>
         </ul>
       </div>
